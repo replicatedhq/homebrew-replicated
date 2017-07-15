82054dc39bf0993129a5c22ac6f6bcce0c4e7d51
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-15 13:22:14 -0700

    replicated version v0.2.2

--- HomebrewFormula/replicated.rb
+++ HomebrewFormula/replicated.rb
@@ -1,9 +1,9 @@
 class Replicated < Formula
   desc "Manage your app's channels and releases from the command line"
-  homepage "https://replicated.com"
-  url "https://github.com/replicatedhq/replicated/releases/download/v0.2.1/replicated_0.2.1_darwin_amd64.tar.gz"
-  version "0.2.1"
-  sha256 "71f1b861036c16bb392381bb8561b70a4b32e23646e2b1f30eca19020142f4c1"
+  homepage "https://www.replicated.com/docs/reference/vendor-cli/"
+  url "https://github.com/replicatedhq/replicated/releases/download/v0.2.2/replicated_0.2.2_darwin_amd64.tar.gz"
+  version "0.2.2"
+  sha256 "1079ebceb383fcde91820cc2560ddf7dc79c3b9aca7d805fad1af584df93041a"
 
   def install
     bin.install "replicated"
