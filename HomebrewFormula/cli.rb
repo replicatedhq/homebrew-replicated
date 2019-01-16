ca5ccc5463393e62f124e7b8896b964f83dbbb64
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-16 15:36:26 -0800

    Brew formula update for cli version v0.5.0

--- HomebrewFormula/cli.rb
+++ HomebrewFormula/cli.rb
@@ -1,15 +1,11 @@
 class Cli < Formula
   desc "Manage your app's channels and releases from the command line"
   homepage "https://www.replicated.com/docs/reference/vendor-cli/"
-  url "https://github.com/replicatedhq/replicated/releases/download/v0.4.0/cli_0.4.0_darwin_amd64.tar.gz"
-  version "0.4.0"
-  sha256 "b0bf24df054bed402e024987cc9207439422b0366180379db513e545a248ad3c"
+  url "https://github.com/replicatedhq/replicated/releases/download/v0.5.0/replicated_0.5.0_darwin_amd64.tar.gz"
+  version "0.5.0"
+  sha256 "9c50ce586635122919510639cc728ff36b1c8f163e5b39bd6cc82b1abb297070"
 
   def install
     bin.install "replicated"
-  end
-
-  test do
-    
   end
 end
