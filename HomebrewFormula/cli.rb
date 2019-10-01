5e71f2c3b622fd4fef947d91f0663b987482a04e
goreleaserbot <goreleaser@carlosbecker.com>
2019-10-01 16:15:28 -0700

    Brew formula update for cli version v0.14.0

--- HomebrewFormula/cli.rb
+++ HomebrewFormula/cli.rb
@@ -2,16 +2,16 @@
 class Cli < Formula
   desc "Manage your app's channels and releases from the command line"
   homepage "https://www.replicated.com/docs/reference/vendor-cli/"
-  version "0.13.0"
+  version "0.14.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/replicatedhq/replicated/releases/download/v0.13.0/replicated_0.13.0_darwin_amd64.tar.gz"
-    sha256 "de4a7c67d1d4a04ab695989f5f44fce746d974c6d18913b2982839286d10a630"
+    url "https://github.com/replicatedhq/replicated/releases/download/v0.14.0/replicated_0.14.0_darwin_amd64.tar.gz"
+    sha256 "7e5b68187c3a8e193485eca9dc4ebd69754d3a986f6cb0b2becd15092cff1464"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/replicatedhq/replicated/releases/download/v0.13.0/replicated_0.13.0_linux_amd64.tar.gz"
-      sha256 "ec037459932af13e50f4ceaacdcbe6aadcefdb1b3fba57dec5ce81c65aaa523b"
+      url "https://github.com/replicatedhq/replicated/releases/download/v0.14.0/replicated_0.14.0_linux_amd64.tar.gz"
+      sha256 "e576ec781494542847b0f7237cc9e50ccaa2edfec4d7e024815d2de145312662"
     end
   end
 
