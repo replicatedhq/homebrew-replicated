f01eabe55af608210c327bc0583282bf153c046e
goreleaserbot <goreleaser@carlosbecker.com>
2023-08-08 17:06:58 -0500

    Brew formula update for cli version v0.54.0

--- HomebrewFormula/cli.rb
+++ HomebrewFormula/cli.rb
@@ -5,11 +5,11 @@
 class Cli < Formula
   desc "Package Replicated applications and manage releases, channels, customers and entitlements using a command-line interface."
   homepage "https://docs.replicated.com/reference/replicated-cli-installing"
-  version "0.53.2"
+  version "0.54.0"
 
   on_macos do
-    url "https://github.com/replicatedhq/replicated/releases/download/v0.53.2/replicated_0.53.2_darwin_all.tar.gz"
-    sha256 "4b3bb540e289ddaea6289f7afcd889f6562bcdec8f72cc220418691880951e64"
+    url "https://github.com/replicatedhq/replicated/releases/download/v0.54.0/replicated_0.54.0_darwin_all.tar.gz"
+    sha256 "19aa62d978e41202f186627218a0874b062cd6ebc6ffc399a24417e4d213c9f6"
 
     def install
       bin.install "replicated"
@@ -18,8 +18,8 @@ class Cli < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/replicatedhq/replicated/releases/download/v0.53.2/replicated_0.53.2_linux_amd64.tar.gz"
-      sha256 "e91ca2a3af5d17d61c5fdd21cbcb5e30ffebf5b851f53e58f11cdb4c9cfd7004"
+      url "https://github.com/replicatedhq/replicated/releases/download/v0.54.0/replicated_0.54.0_linux_amd64.tar.gz"
+      sha256 "5a8b94496fdbe2e82227d0f84ce180e5b2b716734d4df7e68ec1ae1ad86d77f1"
 
       def install
         bin.install "replicated"
